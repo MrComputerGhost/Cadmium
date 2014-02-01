d30560726ad4b9a902611214405df2f8cf5c223f
sci4me <minecraftman7778@gmail.com>
2014-02-01 11:45:28 -0600

    just starting stuffs..

---
+++ src/main/java/com/sci/cadmium/startup/Cadmium.java
@@ -0,0 +1,13 @@
+package com.sci.cadmium.startup;
+
+/**
+ * Cadmium
+ * 
+ * @author sci4me
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+
+public class Cadmium
+{	
+	
+}
